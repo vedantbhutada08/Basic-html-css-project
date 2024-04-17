79cd2b789a67d784381b0bff5808559194d54f62
Vedant Ramesh Bhutada <vedantbhutada08@gmail.com>
2024-04-17 15:34:42 +0530

    Initial commit

---
+++ v.js
@@ -0,0 +1,13 @@
+var marks=prompt("Enter the marks");
+if(marks>=50&&marks<=65){
+document.write("C");
+}
+else if(marks>=66&&marks<=80){
+document.write("B");
+}
+else if(marks>=81&&marks<=90){
+document.write("A");
+}
+else if(marks>=91&&marks<=100){
+document.write("A+");
+}
